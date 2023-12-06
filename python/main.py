78c6065459148980380746a3e860e06814cf271f
zerozipzilch <0zipzilch@gmail.com>
2023-12-06 13:41:15 +0100

    Made import in main dynamic

--- python/main.py
+++ python/main.py
@@ -1,18 +1,19 @@
+import importlib
 import os
 import sys
-
-from tasks.day4.task2 import run as task
 
 def main():
   day_number = sys.argv[1]
   task_number = sys.argv[2]
   input_user = sys.argv[3]
   
-  input_file = 'tasks/day{day_number}/{input_user}-aoc-input.txt'.format(day_number=day_number, task_number=task_number, input_user=input_user)
+  input_file = 'tasks/day{day_number}/{input_user}-aoc-input.txt'.format(day_number=day_number, input_user=input_user)
   input_file = os.path.join(os.path.dirname(__file__), input_file)
 
   with open(input_file) as f:
     input_rows = [line.strip() for line in f.readlines()]
+  
+  task = importlib.import_module('tasks.day{day_number}.task{task_number}'.format(day_number=day_number, task_number=task_number)).run
 
   task(input_rows)
 
