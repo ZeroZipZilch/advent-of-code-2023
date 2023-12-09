b15309c2b448c54cc2e7ed54e9a05cce1d41be90
ZeroZipZilch <0zipzilch@gmail.com>
2023-12-09 13:10:40 +0100

    Finished day 9

---
+++ python/tasks/day9/task1.py
@@ -0,0 +1,36 @@
+import re
+
+def run(input_rows: list[str]):
+  history_book = dict()
+  total_values = 0
+
+  for history_page in input_rows:
+    history_values = list(map(int, history_page.split(' ')))
+    history_book[history_page] = list()
+    
+    history_book[history_page].append(list(history_values))
+
+    while min(history_values) != max(history_values):
+      current_history_values = []
+
+      for (i, value) in enumerate(history_values):
+        if i + 1 < len(history_values):
+          current_history_values.append(history_values[i + 1] - value)
+      
+      history_book[history_page].append(current_history_values)
+      history_values = current_history_values
+
+    next_value = 0
+
+    for i in range(len(history_book[history_page]) - 1, -1, -1):
+      history_page_values = history_book[history_page][i]
+      next_history_page_values = history_book[history_page][i - 1] if i - 1 >= 0 else [0]
+
+      next_value = history_page_values[-1] + next_history_page_values[-1]
+      next_history_page_values.append(next_value)
+
+      # print(f"{i}: +{next_value} - {history_page_values}")
+    
+    total_values += next_value
+
+  print(total_values)
